2f45c73c4ffb357a6a7dabce9915534f5885fb88
implicitdef <implicitdef@users.noreply.github.com>
2015-11-19 17:24:38 +0100

    main code

--- build.sbt
+++ build.sbt
@@ -3,8 +3,8 @@ lazy val root = (project in file(".")).
     name := "scala-http-study",
     version := "0.0.1",
     scalaVersion := "2.11.7",
-    scalacOptions ++= Seq("-unchecked", "-feature", "-deprecation") /*,
-    libraryDependencies += "com.typesafe.play" % "play-ws_2.11" % "2.4.3"
-    ...Add dependencies here...
-    */
+    scalacOptions ++= Seq("-unchecked", "-feature", "-deprecation"),
+    libraryDependencies += "com.typesafe.play" %% "play-ws" % "2.4.3",
+    libraryDependencies += "net.databinder.dispatch" %% "dispatch-core" % "0.11.3",
+    libraryDependencies += "io.spray" %% "spray-client" % "1.3.1"
   )
--- src/main/scala/study/Main.scala
+++ src/main/scala/study/Main.scala
@@ -1,8 +1,83 @@
 package study
 
+
+import scala.concurrent.Future
+
+
 object Main {
 
-  def main(args: Array[String]): Unit =
-    println("Hello world!")
+  def main(args: Array[String]): Unit = {
+    import scala.concurrent.duration.Duration
+    import scala.concurrent.Await
+    Await.result(callWithPlayWs(), Duration.Inf)
+    Await.result(callWithDispatch(), Duration.Inf)
+    Await.result(callWithSprayClient(), Duration.Inf)
+  }
+
+  private def callWithPlayWs(): Future[Unit] = {
+    import play.api.libs.ws.ning.NingWSClient
+    import scala.concurrent.ExecutionContext.Implicits.global
+    val wsClient = NingWSClient()
+    wsClient
+      .url("http://jsonplaceholder.typicode.com/comments/1")
+      .withQueryString("some_parameter" -> "some_value", "some_other_parameter" -> "some_other_value")
+      .withHeaders("Cache-Control" -> "no-cache")
+      .get()
+      .map { wsResponse =>
+        if (! (200 to 299).contains(wsResponse.status)) {
+          sys.error(s"Received unexpected status ${wsResponse.status} : ${wsResponse.body}")
+        }
+        println(s"OK, received ${wsResponse.body}")
+        println(s"The response header Content-Length was ${wsResponse.header("Content-Length")}")
+        wsClient.close()
+      }
+  }
+
+
+  private def callWithDispatch(): Future[Unit] = {
+    import dispatch._
+    import dispatch.Defaults._
+    val h = new Http
+    val requestWithHandler =
+      url("http://jsonplaceholder.typicode.com/comments/1")
+        .<<?(Seq("some_parameter" -> "some_value", "some_other_parameter" -> "some_other_value"))
+        .<:<(Seq("Cache-Control" -> "no-cache"))
+        .OK { response =>
+      println(s"OK, received ${response.getResponseBody}")
+      println(s"The response header Content-Length was ${response.getHeader("Content-Length")}")
+      h.shutdown()
+    }
+    h(requestWithHandler)
+  }
+
+  private def callWithSprayClient(): Future[Unit] = {
+    import akka.actor._
+    import spray.http._
+    import spray.client.pipelining._
+    implicit val actorSystem = ActorSystem()
+    import actorSystem.dispatcher
+    val pipeline = sendReceive
+    pipeline(
+      Get(
+        Uri(
+          "http://jsonplaceholder.typicode.com/comments/1"
+        ).withQuery("some_parameter" -> "some_value", "some_other_parameter" -> "some_other_value")
+      )
+        .withHeaders(HttpHeaders.`Cache-Control`(CacheDirectives.`no-cache`))
+    )
+      .map { response =>
+        if (response.status.isFailure) {
+          sys.error(s"Received unexpected status ${response.status} : ${response.entity.asString(HttpCharsets.`UTF-8`)}")
+        }
+        println(s"OK, received ${response.entity.asString(HttpCharsets.`UTF-8`)}")
+        println(s"The response header Content-Length was ${response.header[HttpHeaders.`Content-Length`]}")
+        actorSystem.shutdown()
+      }
+  }
+
+
+
+
+
 
 }
